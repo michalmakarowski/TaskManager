6207d4453cee897b447e5c932f6d0e7924a2a6e7
michalmakarowski <77726758+michalmakarowski@users.noreply.github.com>
2023-05-19 16:22:02 +0200

    Add TaskManagerContext.cs and TaskModel.cs

---
+++ Models/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskMenager.Models
+{
+    [Table("Tasks")]
+    public class TaskModel
+    {
+        [Key]
+        public int TaskID { get; set; }
+        [DisplayName("Nazwa")]
+        [Required(ErrorMessage ="Pole jest wymagane")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [DisplayName("Opis")]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
