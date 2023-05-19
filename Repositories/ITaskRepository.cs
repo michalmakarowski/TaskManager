47a0fead8c9b5c7908c5f4839c338a96003751a5
michalmakarowski <77726758+michalmakarowski@users.noreply.github.com>
2023-05-19 16:22:53 +0200

    Add ITaskRepository.cs and TaskRepository.cs

---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskMenager.Models;
+
+namespace TaskMenager.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskId);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+    }
+}
---
+++ Repositories/TaskRepository.cs
@@ -0,0 +1,45 @@
+﻿using TaskMenager.Models;
+
+namespace TaskMenager.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly TaskManagerContext _context;
+        public TaskRepository(TaskManagerContext context)
+        {
+            _context = context;
+        }
+        public TaskModel Get(int taskId) => _context.Tasks.SingleOrDefault(x => x.TaskID == taskId);
+
+
+        public IQueryable<TaskModel> GetAllActive() => _context.Tasks.Where(x => !x.Done);
+        public void Add(TaskModel task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+        public void Update(int taskId, TaskModel task)
+        {
+            var result = _context.Tasks.SingleOrDefault(x => x.TaskID == taskId);
+            if (result != null)
+            {
+                result.Name = task.Name;
+                result.Description = task.Description;
+                result.Done = task.Done;
+
+                _context.SaveChanges();
+            }
+        }
+        public void Delete(int taskId)
+        {
+            var result = _context.Tasks.SingleOrDefault(x => x.TaskID == taskId);
+            if (result != null)
+            {
+                _context.Tasks.Remove(result);
+                _context.SaveChanges();
+            }
+        }
+
+       
+    }
+}
